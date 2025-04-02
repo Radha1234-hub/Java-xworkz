2192ef3156ae75e969b88954319a8caa81bfd154
Your Name <you@example.com>
2025-04-02 11:54:48 +0530

    adding files

--- Pencil.java
+++
@@ -1,9 +0,0 @@
-class Pencil{
-public static void main(String args[]){
-System.out.println("About this item");
-System.out.println("One 40-count pack of BIC Xtra-Smooth Bright Edition No. 2 Mechanical Pencils With Erasers");
-System.out.println("Versatile 0.7mm medium point is ideal for a variety of everyday writing activities and a great addition to school supplies");
-System.out.println("BIC pencils with high-quality lead that doesn’t smudge and erases cleanly, so your work looks neat and professional");
-System.out.println("These lead pencils are always ready and never need sharpening; to advance the lead, simply click the built-in eraser");
-System.out.println("Great automatic pencil for school, with No. 2 lead that’s compatible with most standardized tests");
-}}
--- Tape.java
+++
@@ -1,9 +0,0 @@
-class Tape{
-public static void main(String args[]){
-System.out.println("About the item");
-System.out.println("PERFECT GIFT PRESENTATION: TAFOMEGA invisible tape blends seamlessly with the gift wrap, ensuring a polished and pristine appearance that showcases the original beauty of your gift");
-System.out.println("SIMPLE SLICING: The TAFOMEGA invisible tape dispenser is equipped with stainless steel blades with sharp serrated edges. This ensures that the gift tape is easily peeled off, providing you with a smooth and frustration-free experience");
-System.out.println("ERGONOMIC DESIGN: TAFOMEGA invisible tape is equipped with an ergonomically designed handheld tape dispenser, ensuring your ease and comfort during use");
-System.out.println("STRONG ADHESION: The invisible tape for school ensures a secure adhesive bond, allowing you to effortlessly solve various minor problems at school, Office, and Home");
-System.out.println("TAPE DIMENSIONS: You'll receive 12 rolls of invisible wrapping tape rolls with dispenser, each tape measures  catering to your everyday needs");
-}}
